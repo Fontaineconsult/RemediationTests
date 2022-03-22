acdbbdca54050364f204d3b328609d0829ad4233
Daniel Fontaine <fontaine@sfsu.edu>
2022-03-22 09:17:37 -0700

    sdfds

--- pdfValidation.py
+++ pdfValidation.py
@@ -1,6 +1,9 @@
 from pdfminer import high_level
 import pdfminer
 
+from pikepdf import Pdf, Page, PdfImage, OutlineItem
+
+
 
 def page_contains_text(page):
     if page.groups is None:
@@ -60,3 +63,54 @@ def pdf_status(document_location):
         return 2  # Only Image of Text
     if t1 == 0 and t2 == 0:
         return 3  # No image of text and no page text
+
+
+
+
+def test(document_location):
+
+    pdf = Pdf.open(document_location)
+
+
+    # print(pdf.pages[0])
+    # imgs = pdf.pages[0].images['/Im0']
+    #
+    # obj = PdfImage(imgs)
+    # print(obj.colorspace)
+    # print((pdf.pages[0].get("/Resources").get("/Font")))
+    # print(dir(pdf.pages[0].Resources.XObject.Im1))
+    # print(pdf.pages[0].Resources.XObject.Im1.to_json())
+
+    # print(dir(pdf.Root.get('/StructTreeRoot')))
+    #
+    # zerk = pdf.Root.get('/StructTreeRoot')
+    # print(zerk.read_bytes())
+    # print(dir(zerk.ParentTree))
+    # print(dir(zerk.ParentTree.items()))
+    # print(root[3])
+
+    root1 = pdf.Root.get("/Outlines")
+    # print(list(root1))
+    # print(list(root1.get("/First").get("/First").get("/SE").get("/K")[0].get("/K")[0]))
+    # print(root1.get("/First").get("/First").get("/SE").get("/K")[0].get("/K")[0].get("/Alt"))
+
+    # print(list(root1))
+    # print(list(root1["/First"]))
+    # print(list(root1["/First"]["/First"]))
+    # print(list(root1["/First"]["/First"]["/SE"]))
+    print((root1['/First']['/First']['/SE']['/K'][1]['/P']['/K'][0]['/K'][0]['/Pg']['/Resources']['/XObject']['/Im1']['/ID']))
+
+
+    # print(len(list(root1.get("/First").get("/First").get("/SE").get("/K"))))
+    #
+    # tst = list(root1.get("/First").get("/First").get("/SE").get("/K"))[0]
+
+
+
+
+
+    # print(lorp.items)
+
+
+test(r"Z:\ACRS\project_files\ab780356aac11e379d56431b4f3a5de454e2910ce46d52f0804b21481d7d09de\source\Number Sense -Ed Leadership.pdf")
+
