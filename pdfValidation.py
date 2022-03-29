a488698b8a9310a2f0af5b489a14698561e0722f
Daniel Fontaine <fontaine@sfsu.edu>
2022-03-29 09:16:23 -0700

    sdfds

--- pdfValidation.py
+++ pdfValidation.py
@@ -4,7 +4,6 @@ from pikepdf import Pdf, Dictionary, Array, String, Object, Name
 
 
 
-
 def page_contains_text(page):
     if page.groups is None:
         return False
@@ -83,63 +82,109 @@ def check_if_tagged(document):
             return False
 
 
+
+
 def check_for_alt_tags(document):
 
+
+
+
     root = document.Root.get("/StructTreeRoot")
+    roleMap = root.get("/RoleMap")
     document_photos = list()
+    IDDict = {}
+    print(repr(roleMap))
     if not check_if_tagged(document):
         raise Exception("PDF Not Tagged")
-    print(repr(root))
-    def recurse(node):
+    # print(repr(document.Root.get("/Names")))
+
+
+    def recurse_a_node(node, parent):
+
+
 
         if isinstance(node, Dictionary):
+
+
+            if "/BBox" in node.keys():
+
+                if "/Pg" in parent.keys():
+                    global BboxCount
+                    BboxCount += 1
+                    if '/Alt' in parent.keys():
+                        document_photos.append(True)
+                    else:
+                        document_photos.append(False)
             if '/K' in node.keys():
-                next_node = node.get('/K')
-                recurse(next_node)
-            if '/A' in node.keys():
-                next_node = node.get('/A')
-                recurse(next_node)
+                recurse_k_nodes(node)
+
         if isinstance(node, Array):
-            for item in node:
-                if isinstance(item, Dictionary):
-                    print("DICTIONARY")
-                if isinstance(item, Array):
-                    print("Arrayyyy")
+
+            for each in node:
+                recurse_a_node(each, parent)
 
 
-                if isinstance(item, Dictionary):
+    def recurse_s_node(node):
 
-                    # if '/Alt' in item.keys():
-                    #     if '/S' in item.keys():
-                    #         print(repr(item.get("/S")))
-                    #         print(repr(item.get("/Pg")))
+        def check_xObject():
 
-                    if "/A" in item.keys():
-                        try:
-                            if '/BBox' in item.get('/A'):
-                                if '/Alt' in item.keys():
-                                    document_photos.append(True)
-                                else:
-                                    document_photos.append(False)
+            resources = node.get('/Pg').get("/Resources")
+            if "/XObject" in resources.keys():
+                XObject = resources.get("/XObject")
 
-                        except TypeError:
-                            if String('/BBox') in item.get('/A'):
-                                if '/Alt' in item.keys():
-                                    document_photos.append(True)
-                                else:
-                                    document_photos.append(False)
+                for key in XObject.keys():
+                    if XObject[key].get("/Subtype") == "/Image":
+                        derived_id = XObject[key].get("/Height") + XObject[key].get("/Width") + XObject[key].get("/Length")
+                        IDDict[derived_id] = [key]
 
-                    if "/K" in item.keys():
-                        next_node = item.get('/K')
-                        recurse(next_node)
+                        if "/Alt" in node.keys() and len(str(node.get('/Alt'))) > 0:
+                            print("NODE", node.keys(), len(str(node.get('/Alt'))))
+                            IDDict[derived_id] = True
+                        else:
+                            IDDict[derived_id] = False
 
-                if isinstance(item, Array):
+        if roleMap is not None and len(roleMap.keys()) > 0:
+            try:
+                if roleMap[node.get('/S')] == Name("/Figure"):
+                    check_xObject()
+            except KeyError:
+                if node.get('/S') == Name("/Figure"):
+                    check_xObject()
+        else:
+            if node.get('/S') == Name("/Figure"):
+                check_xObject()
 
-                    print("___________ array in array")
-                    recurse(item)
+    def recurse_k_nodes(node):
 
+        if isinstance(node, Dictionary):
+            if "/K" in node.keys():
+                recurse_k_nodes(node.get('/K'))
+            if "/A" in node.keys():
+                recurse_a_node(node.get("/A"), node)
+            if "/S" in node.keys():
+                recurse_s_node(node)
 
-    recurse(root)
+        if isinstance(node, Array):
+            for each in node:
+                if isinstance(each, Dictionary):
+                    print((each.keys()))
+                    if "/K" in each.keys():
+                        recurse_k_nodes(each.get("/K"))
+
+                    # if "/A" in each.keys():
+                    #     recurse_a_node(each.get("/A"), each)
+
+                    if "/S" in each.keys():
+                        recurse_s_node(each)
+
+                if isinstance(each, Array):
+                    recurse_k_nodes(each)
+
+    recurse_k_nodes(root)
+
+    for figure in IDDict:
+        document_photos.append(IDDict[figure])
+
     return document_photos
 
 
@@ -177,7 +222,7 @@ def pdf_check(location):
 
     obj = {
 
-        "tagged": tagged,
+        "tagged": bool(tagged),
         "alt_tag_count": alt_tag_count,
         "pdf_text_type": pdf_text_type,
         "metadata": check_metadata(Pikepdf)
@@ -188,3 +233,5 @@ def pdf_check(location):
 
 # print(pdf_check(r"Z:\ACRS\project_files\ae37b2abbe4bd48244c604f602464fdca6563f8c6378e53e27430d55b8638fd5\source\EDD 786-07 Sp22 Syllabus.pdf"))
 print(pdf_check(r"Z:\ACRS\project_files\1f2b9c41f10c0dcbc69ffe9adc6c03ee55bba87c70fe687213c4ebccb56284ff\source\Blooms Taxonomy for Teaching Lesson Design.pdf"))
+# print(pdf_check(r"Z:\ACRS\project_files\8b8999c25661c10085d0ababe7050af301ebc78f9dbb03edeeab187db0424aca\source\EdTPA Making Good Choices 21-22.pdf"))
+# print(pdf_check(r"Z:\ACRS\project_files\fdf547da4d49ff8410276d99cbece4cd9b6afe2dd5f2636bcb7dd2357f933e1b\source\Ladder-of-Inference-Overview.pdf"))
